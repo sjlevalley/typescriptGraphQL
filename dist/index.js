3929604d3d0209773669ed0953d1ff5ab62fb164
Steve LeValley <slevalley@gmail.com>
2022-08-24 07:59:09 -0700

    can now create Posts and persist to DB

--- dist/index.js
+++ dist/index.js
@@ -4,18 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
-const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const path_1 = __importDefault(require("path"));
 const main = async () => {
-    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    const orm = await core_1.MikroORM.init({
+        migrations: {
+            path: path_1.default.join(__dirname, "./migrations"),
+        },
+        entities: [Post_1.Post],
+        dbName: "lireddit",
+        user: "postgres",
+        password: "postgres",
+        debug: !constants_1.__prod__,
+        type: "postgresql",
+    });
     await orm.getMigrator().up();
     const em = orm.em.fork();
-    const post = {
+    const post = em.create(Post_1.Post, {
         title: "My First Post",
         updatedAt: new Date(),
         createdAt: new Date(),
-    };
-    await em.create(Post_1.Post, post);
+    });
     const posts = await em.find(Post_1.Post, {});
     console.log(posts);
 };
--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,38 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import { EntityManager } from "@mikro-orm/postgresql";
+import type { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import path from "path";
 
 const main = async () => {
-  const orm = await MikroORM.init(mikroConfig);
-  // const orm = await MikroORM.init<PostgreSqlDriver>({
-  //   migrations: {
-  //     path: path.join(__dirname, "./migrations"),
-  //     // pathTs: "src/migrations",
-  //     // pattern: /^[\w-]+\d+\.[tj]s$/,
-  //   },
-  //   //   entities: ["./dist/entities/**/*.js"], // path to our JS entities (dist), relative to `baseDir`
-  //   //   entitiesTs: ["./src/entities/**/*.ts"],
-  //   entities: [Post],
-  //   dbName: "lireddit",
-  //   user: "postgres",
-  //   password: "postgres",
-  //   debug: !__prod__,
-  //   type: "postgresql",
-  // });
+  // const orm = await MikroORM.init(mikroConfig);
+  const orm = await MikroORM.init<PostgreSqlDriver>({
+    migrations: {
+      path: path.join(__dirname, "./migrations"),
+      // pathTs: "src/migrations",
+      // pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    //   entities: ["./dist/entities/**/*.js"], // path to our JS entities (dist), relative to `baseDir`
+    //   entitiesTs: ["./src/entities/**/*.ts"],
+    entities: [Post],
+    dbName: "lireddit",
+    user: "postgres",
+    password: "postgres",
+    debug: !__prod__,
+    type: "postgresql",
+  });
   await orm.getMigrator().up();
-
   const em = orm.em.fork();
-  const post = {
+
+  const post = em.create(Post, {
     title: "My First Post",
     updatedAt: new Date(),
     createdAt: new Date(),
-  };
+  });
 
   // Add to database
-  await em.create(Post, post);
+  // em.persistAndFlush(post);
 
   const posts = await em.find(Post, {});
   console.log(posts);
