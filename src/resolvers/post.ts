9de9f03922c4cf2a985ff5376905f834545f3094
Steve LeValley <slevalley@gmail.com>
2022-10-16 12:24:27 -0700

    Stopped at 11:00:00, user can edit and delete own posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -180,23 +180,45 @@ export class PostResolver {
   }
 
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
     @Arg("id", () => Int) id: number,
-    @Arg("title", () => String, { nullable: true }) title: string
+    @Arg("title") title: string,
+    @Arg("text") text: string,
+    @Ctx() { req }: MyContext
   ): Promise<Post | null> {
-    const post = await Post.findOneBy({ id });
-    if (!post) {
-      return null;
-    }
-    if (typeof title !== "undefined") {
-      await Post.update({ id }, { title });
-    }
-    return post;
+    const result = await typormConnection
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title, text })
+      .where('id = :id and "creatorId" = :creatorId', {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning("*")
+      .execute();
+
+    return result.raw[0];
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg("id") id: number): Promise<boolean> {
-    await Post.delete(id);
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { req }: MyContext
+  ): Promise<boolean> {
+    // NOT CASCADING DELETE WAY
+    // const post = await Post.findOneBy({ id });
+    // if (!post) {
+    //   return false;
+    // }
+    // if (post?.creatorId !== req.session.userId) {
+    //   throw new Error("not authorized");
+    // }
+    // await Updoot.delete({ postId: id });
+    // await Post.delete({ id });
+
+    // CASCADING DELETE WAY (onDelete: 'CASCADE' added as an option to the entity)
+    await Post.delete({ id, creatorId: req.session.userId });
     return true;
   }
 }
