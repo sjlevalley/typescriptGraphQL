c98af498d866d4de523af37f24fcb1bb737b1772
Steve LeValley <slevalley@gmail.com>
2022-12-05 08:11:35 -0800

    update

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ declare module "express-session" {
 export const typormConnection = new DataSource({
   type: "postgres",
   // url: `${process.env.DATABASE_URL}`,
-  url: "postgresql://postgres:postgres@host.docker.internal:5432/lireddit2",
+  url: "postgresql://postgres:postgres@127.0.0.1:5432/reddit-ish",
   logging: true,
   // synchronize: true,
   entities: [Post, User, Vote],
@@ -66,7 +66,8 @@ const main = async () => {
   // await Post.delete({}); // delete posts from DB
 
   const app = express();
-  const redis = new Redis({ host: "redis" });
+  // const redis = new Redis({ host: "redis" });
+  const redis = new Redis();
 
   let RedisStore = connectRedis(session);
 
